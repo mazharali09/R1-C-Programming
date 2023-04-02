4f84a37a1d626025f3696d8f2ce74e1a917b20e6
Mazhar Ali Linux <mazharali09>
2023-04-03 03:21:15 +0500

    Updated lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -4,16 +4,41 @@
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
     /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
-}
+    unsigned temp = 1 << n;
+    unsigned bit = x & temp;
+    bit = bit >> n;
 
+    return bit; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+}
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
     /* YOUR CODE HERE */
+    unsigned temp = (1 << n);
+
+    if (v == 0) {
+        /* nth bit of x is made 0 with a bitwise AND operation */
+        *x &= ~temp;
+    } 
+    else {
+        /* nth bit of x is made 1 with a bitwise OR operation */
+        *x |= temp;
+    }
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
     /* YOUR CODE HERE */
+    unsigned temp = 1 << n;
+    unsigned bit = *x & temp;
+    bit = bit >> n;
+    
+    if (bit == 0){
+        /* bit is fliped and made 1 */
+        *x |= temp;
+    }
+    else{
+	/* bit is filped and made 0 */
+	*x &= ~temp;
+    }
 }
 
--- lab02/linked_list.c
+++ lab02/linked_list.c
@@ -4,6 +4,7 @@
 
 /* returns a new node whose data is set to DATA and next is set to NULL */
 Node *create_node(int data) {
+    /* Don't worry about malloc yet! It is not in the scope of this lab */
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         perror("Malloc failed\n");
@@ -13,6 +14,7 @@ Node *create_node(int data) {
     return new_node;
 }
 
+/* Don't worry about free(), it is not in the scope of this lab */
 /* Frees the list starting at HEAD */
 void free_list(Node *head) {
     while (head != NULL) {
@@ -52,8 +54,8 @@ void print_list(struct Node *head) {
 }
 
 /* Iteratively reverses a linked list whose first node is HEAD */
-void reverse_list(struct Node **head) {
-    if (head == NULL) {
+void reverse_list(struct Node **head){
+    if (head == NULL || *head == NULL) {
         return;
     }
     struct Node *curr = *head;
@@ -71,10 +73,16 @@ void reverse_list(struct Node **head) {
 /* Creates a new node with a data field set to DATA and adds the node
    to the back of the list pointed to by HEAD */
 void add_to_back(Node **head, int data) {
-    if (head == NULL) {
+    
+    Node *new_node = create_node(data);
+    if (new_node == NULL) {
         return;
     }
-    Node *new_node = create_node(data);
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    } 
+
     Node *prev;
     for (Node *curr = *head; curr != NULL; curr = curr->next) {
         prev = curr;
